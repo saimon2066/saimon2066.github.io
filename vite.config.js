172703585696aa62ff9b4db7baa27a1e1b6efef8
Šimon Mariaš <193417075+saimon2066@users.noreply.github.com>
2026-09-13 22:51:41 +0200

    Base fix

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,5 @@ import react from '@vitejs/plugin-react';
 // Wrong base = blank page with 404s on the JS/CSS files.
 export default defineConfig({
   plugins: [react()],
-  base: '/saimon2066.github.io'
+  base: '/'
 });
